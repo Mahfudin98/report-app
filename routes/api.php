330c74947200d5d2df82c22dce1d21032216a257
Mahfudin98 <mahfudinnawawi@gmail.com>
2022-12-15 09:57:07 +0700

    add middelware in target controller

--- routes/api.php
+++ routes/api.php
@@ -25,24 +25,25 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::controller(RajaOngkirController::class)->group(function(){
+
+Route::controller(RajaOngkirController::class)->group(function () {
     Route::get('/provinsi', 'provinsi')->name('provinsi');
     Route::get('/kota/{id}', 'kota')->name('kota');
     Route::get('/kecamatan/{id}', 'kecamatan')->name('kecamatan');
     Route::post('/cek-ongkir', 'cekOngkir')->name('cek-ongkir');
 });
 
-Route::controller(ApiLoginController::class)->group(function(){
+Route::controller(ApiLoginController::class)->group(function () {
     Route::post('login', 'login');
-    Route::middleware(['auth:sanctum'])->group(function(){
+    Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('/user-login', 'userLogin');
         Route::post('/logout', 'logout');
         Route::get('/user-image', 'userImage');
     });
 });
 
-Route::controller(ApiUsersController::class)->group(function(){
-    Route::middleware(['auth:sanctum'])->group(function(){
+Route::controller(ApiUsersController::class)->group(function () {
+    Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('/user-list', 'index');
         Route::get('/user-show/{username}', 'show');
         Route::get('/user-edit', 'edit');
@@ -51,14 +52,14 @@ Route::controller(ApiUsersController::class)->group(function(){
     });
 });
 
-Route::controller(ApiDivisionsController::class)->group(function(){
-    Route::middleware(['auth:sanctum'])->group(function(){
+Route::controller(ApiDivisionsController::class)->group(function () {
+    Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('/division-list', 'index');
     });
 });
 
-Route::controller(ApiTransactionController::class)->group(function(){
-    Route::middleware(['auth:sanctum'])->group(function(){
+Route::controller(ApiTransactionController::class)->group(function () {
+    Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('/transaction-index', 'index');
         Route::get('/transaction-show/{code}', 'show');
         Route::post('/transaction-store', 'transactionStore');
@@ -68,16 +69,16 @@ Route::controller(ApiTransactionController::class)->group(function(){
     });
 });
 
-Route::controller(ApiProductsController::class)->group(function(){
-    Route::middleware(['auth:sanctum'])->group(function(){
+Route::controller(ApiProductsController::class)->group(function () {
+    Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('/product-index', 'index');
         Route::get('/product-category', 'category');
         Route::get('/product-select', 'selectProduct');
     });
 });
 
-Route::controller(ApiMembersController::class)->group(function(){
-    Route::middleware(['auth:sanctum'])->group(function(){
+Route::controller(ApiMembersController::class)->group(function () {
+    Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('/member-index-cs', 'indexCS');
         Route::get('/member-select', 'selectMember');
         Route::post('/member-store', 'store');
@@ -86,21 +87,21 @@ Route::controller(ApiMembersController::class)->group(function(){
     });
 });
 
-Route::controller(ProfileDashboardController::class)->group(function(){
-    Route::middleware(['auth:sanctum'])->group(function(){
+Route::controller(ProfileDashboardController::class)->group(function () {
+    Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('/profile-bar/{username}', 'barChart');
     });
 });
 
-Route::controller(ActivityUserController::class)->group(function(){
-    Route::middleware(['auth:sanctum'])->group(function(){
+Route::controller(ActivityUserController::class)->group(function () {
+    Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('/user-activity/{username}', 'userActivity');
         Route::get('/user-activity-list/{id}', 'userActivityDetail');
     });
 });
 
-Route::controller(DashboardIndex::class)->group(function(){
-    Route::middleware(['auth:sanctum'])->group(function(){
+Route::controller(DashboardIndex::class)->group(function () {
+    Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('/line-chart-dashboard', 'lineChart');
         Route::get('/top-cs', 'topCS');
         Route::get('/top-product', 'topProduk');
@@ -108,8 +109,8 @@ Route::controller(DashboardIndex::class)->group(function(){
     });
 });
 
-Route::controller(DashboardController::class)->group(function(){
-    Route::middleware(['auth:sanctum'])->group(function(){
+Route::controller(DashboardController::class)->group(function () {
+    Route::middleware(['auth:sanctum'])->group(function () {
         Route::get('/owner-chart-dashboard', 'lineChart');
         Route::get('/owner-bar-dashboard', 'barChart');
         Route::get('/owner-top-cs', 'topCS');
@@ -120,9 +121,11 @@ Route::controller(DashboardController::class)->group(function(){
     });
 });
 
-Route::controller(TargetController::class)->group(function(){
-    Route::get('/owner-target-adv', 'advList');
-    Route::get('/owner-target-index', 'index');
-    Route::post('/owner-target-store', 'store');
-    Route::get('/owner-target-persen', 'percent');
+Route::controller(TargetController::class)->group(function () {
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::get('/owner-target-adv', 'advList');
+        Route::get('/owner-target-index', 'index');
+        Route::post('/owner-target-store', 'store');
+        Route::get('/owner-target-persen', 'percent');
+    });
 });
