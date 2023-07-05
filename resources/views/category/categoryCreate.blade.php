f72279c4942e19b5c9b57b06b265a25aacecd195
Mahfudin98 <mahfudinnawawi@gmail.com>
2023-07-05 09:33:13 +0700

    update category select create and edit

--- resources/views/category/categoryCreate.blade.php
+++ resources/views/category/categoryCreate.blade.php
@@ -5,11 +5,13 @@
                 @csrf
                 <div class="mb-3">
                     <label for="category_name" class="form-label">Category Name</label>
-                    <input id="category_name" type="text" class="form-control rounded-lg" name="category_name" placeholder="Category Name" aria-label="category_name" aria-describedby="basic-addon1">
+                    <input id="category_name" type="text" class="rounded-lg form-control" name="category_name"
+                        placeholder="Category Name" aria-label="category_name" aria-describedby="basic-addon1">
                 </div>
                 <div class="mb-3">
                     <label for="category_type" class="form-label">Category Type</label>
-                    <select id="category_type" class="form-select rounded-lg" aria-label="Default select example" name="category_type">
+                    <select id="category_type" class="rounded-lg form-select" aria-label="Default select example"
+                        name="category_type">
                         <option selected>Pilih Kategori Tipe</option>
                         <option value="bpom">BPOM</option>
                         <option value="farma">Farma</option>
@@ -17,15 +19,15 @@
                 </div>
                 <div class="mb-3">
                     <label for="category_pay" class="form-label">Category Pay</label>
-                    <select id="category_pay" class="form-select rounded-lg" aria-label="Default select example" name="category_pay">
+                    <select id="category_pay" class="rounded-lg form-select" aria-label="Default select example"
+                        name="category_pay">
                         <option selected>Pilih Kategori Produk</option>
-                        <option value="paket komplit">Paket Komplit</option>
-                        <option value="paket hemat">Paket Hemat</option>
+                        <option value="paket">Paket</option>
                         <option value="ecer">Ecer</option>
                     </select>
                 </div>
                 <div class="mb-3">
-                    <button class="btn btn-info text-white">Submit</button>
+                    <button class="text-white btn btn-info">Submit</button>
                 </div>
             </form>
         </div>
